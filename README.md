a51e5608e8d6f08fc190905ca9114e512244a200
Thibault Laurens <laurens.thibault@gmail.com>
2014-04-06 18:41:30 +0100

    aws.jobs.js query aws and emit event for dashing OK

--- README.md
+++ README.md
@@ -12,24 +12,44 @@ AWS dashboard powered by Dashing-js, Redis & Node.js
 - go and check the dashboard at http://localhost:3030
 
 
+### Supported components:
+- EC2 instance
+- Elastic Load Balancer
+- Security Group
+- S3 Bucket
+- Elastic IP
+
+
 ### Notes
 
 - JOB_REFRESH property needs 'cron' like syntax
-- max elbs value is hardcoded in aws.jade to 20 (default)
-
+- Values for 'data-max' property of Meters widget:
+    - EC2 instances: API call
+    - Load Balancers: 20
+    - Elastic IPs: API call
 
 
 ### TODO
 
 - 1) ~~config.js (redis_url, job_interval, aws_account)~~
-- 2) aws.jobs.js query aws and emit event for dashing
+- 2) ~~aws.jobs.js query aws and emit event for dashing~~
 - 3) cron job for aws.job.js
 - 4) design aws.jade and awstv.jade
 - 5) store aws in redis, aws.jobs.js write to redis
 
+#### Other default values to implement:
+- RDS: 40
+- R53
+    - Hosted zones: 500
+    - Records: 10,000 / HZ
+- EC:
+    - Nodes: 20
+    - SG: NC
+
 
 ### NEXT
 
+- play with pricing API
 - handle redis connection drop + add opt redis auth
 - api.js -> read redis and send json info to browser at /api/ec2, /api/ etc.
 - change the job_interval in the UI
--- jobs/aws.jobs.js
+++ jobs/aws.jobs.js
@@ -12,6 +12,10 @@ AWS.config.update(
 var ec2 = new AWS.EC2();
 var elb = new AWS.ELB();
 var s3 = new AWS.S3();
+var rds = new AWS.RDS();
+var elasticache = new AWS.ElastiCache();
+var route53 = new AWS.Route53();
+
 
 ec2.describeAccountAttributes(function(err, data){
     if (err) console.log(err);
@@ -55,20 +59,68 @@ elb.describeLoadBalancers(function(err, data) {
     send_event('elb', {value: data.LoadBalancerDescriptions.length })
 });
 
+
 s3.listBuckets(function(err, data) {
     if (err) console.log(err, err.stack);
     else console.log("s3 buckets: " + data.Buckets.length);
     send_event('s3', {text: data.Buckets.length })
 });
 
-send_event('summary', { items: [
-        { label:"EC2 instances", value:"15" },
-        { label:"Elastic IPs", value:"2" },
-        { label:"Elastic Load Balancers", value:"3" },
-        { label:"S3 Buckets", value:"1" },
-        { label:"Security Groups", value:"32" }
-    ] })
 
-//todo limits api
-//todo pricing api
+rds.describeDBInstances(function(err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log('rds: ' + data.DBInstances.length);
+    send_event('rds', {text: data.DBInstances.length })
+});
+
+
+elasticache.describeCacheClusters(function (err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log('cache clusters: ' + data.CacheClusters.length);
+    send_event('cache_cluster', {text: data.CacheClusters.length });
+});
+
+elasticache.describeCacheSecurityGroups(function (err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log('cache sg: ' + data.CacheSecurityGroups.length);
+    send_event('cache_sg', {text: data.CacheSecurityGroups.length });
+});
+
+elasticache.describeReservedCacheNodes(function(err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log("cache nodes: " + data.ReservedCacheNodes.length);
+    send_event('cache_node', {text: data.ReservedCacheNodes.length });
+});
+
+
+route53.listHostedZones(function(err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log('hosted zones: ' + data.HostedZones.length);
+    send_event('hz', {current: data.HostedZones.length })
+
+    var records = 0
+    data.HostedZones.forEach(function(hz){
+        route53.listResourceRecordSets({HostedZoneId: hz.Id}, function (err, data){
+            if (err) console.log(err, err.stack);
+            else records += data.ResourceRecordSets.length
+            send_event('record', {current: records })
+        })
+    });
+
+});
+
+
+send_event('summary', { items: [
+        { label:"EC2 instances", value:"0" },
+        { label:"Security Groups", value:"0" },
+        { label:"Elastic Load Balancers", value:"0" },
+        { label:"Elastic IPs", value:"0" },
+        { label:"S3 Buckets", value:"0" },
+        { label:"RDS instances", value:"0" },
+        { label:"R53 zones", value:"0" },
+        { label:"R53 records", value:"0" },
+        { label:"ElastiCache clusters", value:"0" },
+        { label:"ElastiCache nodes", value:"0" },
+        { label:"ElastiCache SG", value:"0" }
+    ] })
 
