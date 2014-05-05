f1f50e9a001e70eed600a2ab1370be5a9cafc0a1
Thibault Laurens <laurens.thibault@gmail.com>
2014-05-05 15:29:39 +0100

    DAILY_JOB_INTERVAL

--- config.ex.js
+++ config.ex.js
@@ -2,6 +2,7 @@ module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     JOB_INTERVAL: "*/30 * * * * *",
     LONG_JOB_INTERVAL:  "0 */30 * * * *",
+    DAILY_JOB_INTERVAL: "0 0 00 * * *",
     AWS: {
         ACCESSKEY: "lalalalala",
         SECRETKEY: "lalalalala"
